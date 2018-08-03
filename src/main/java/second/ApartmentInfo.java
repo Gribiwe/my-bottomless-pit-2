9137275bd14236aacb4d6706aaa557d89331635c
unknown <grib.morpech@gmail.com>
2018-08-03 12:11:08 +0300

    javadoc for apartment info

--- src/main/java/second/ApartmentInfo.java
+++ src/main/java/second/ApartmentInfo.java
@@ -3,32 +3,28 @@ package second;
 import java.math.BigInteger;
 
 /**
- * Created by nikit on 16.07.2018.
+ * Class for representation of
+ * apartment's information about
+ * entrance and floor of apartment
  */
 public class ApartmentInfo {
-
 
     private BigInteger floor;
     private BigInteger entrance;
 
-    public ApartmentInfo(String floor, String entrance) {
-        this.floor = new BigInteger(floor);
-        this.entrance = new BigInteger(entrance);
-    }
-
+    /**
+     * Translates information of
+     * apartment's floor and entrance into
+     * composite apartment information
+     *
+     * @param floor number of apartment's floor
+     * @param entrance number of apartment's entrance
+     */
     public ApartmentInfo(BigInteger floor, BigInteger entrance) {
         this.floor = floor;
         this.entrance = entrance;
     }
 
-    public void setFloor(BigInteger floor) {
-
-        this.floor = floor;
-    }
-
-    public void setEntrance(BigInteger entrance) {
-        this.entrance = entrance;
-    }
 
     public BigInteger getFloor() {
 
